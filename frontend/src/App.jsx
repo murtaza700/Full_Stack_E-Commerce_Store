f71ec4126c66df5f102aa0280bcd1ea7824d8cd6
Ghulam Murtaza <murtazakasur7@gmail.com>
2026-07-11 23:46:12 +0500

    Client Url allow in app.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -47,9 +47,6 @@ const App = () => {
   const dispatch = useDispatch();
   const { isAuthenticated } = useSelector(state => state.auth);
 
-  const API = import.meta.env.VITE_BASE_API;
-  console.log(API)
-
   useEffect(() => {
     dispatch(loadUser());
   }, [dispatch]);
